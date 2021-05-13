4aa93f89a3025742e84624bbfc0f5532a27f0f9d
Marc Jauvin <marc.jauvin@gmail.com>
2021-05-13 14:57:18 -0400

    add missing menu_label localizaion string

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+    'settings' => [
+        'menu_label' => 'SSO Identity Provider',
+    ],
     'fields' => [
         'name'         => 'Service Provider Name',
         'domain'       => 'Service Provider Domain',
